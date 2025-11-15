1a762d7c3cb25fcddf890f53406ab0ef7af12f50
mlucasmichel <mlucasmichel14@gmail.com>
2025-11-15 18:57:37 +0000

    Update SECRET_KEY to use environment variable for production security

--- financial_map/settings.py
+++ financial_map/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7h!v!sml93qls1ld)=2i=#v69xiq(y7y6ztw3+!y*z00op7tjf'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
