174573d94c7238afdc62de28bd708ded949faf31
mlucasmichel <mlucasmichel14@gmail.com>
2025-11-15 20:44:02 +0000

    Add initial migration for Category model with fields and options

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# -- Category Model -- #
+class Category(models.Model):
+    CATEGORY_TYPE_CHOICES = [
+        ('I', 'Income'),
+        ('E', 'Expense'),
+    ]
+
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False
+    )
+
+    type = models.CharField(
+        max_length=1,
+        choices=CATEGORY_TYPE_CHOICES,
+        default='E',
+        null=False
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
